7269e9f093638b28371568d1ef70a7c2f63c3fd2
EngiFire <97035562+EngiFire@users.noreply.github.com>
2023-12-23 12:32:06 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
-// Import the express module
 const express = require('express');
 
-// Create an instance of express
 const app = express();
 
-// Define a route
 app.get('/', (req, res) => {
   res.send('Так - это просто заглушка для выполнения задания и не более. Работу выполнил Волощенко Алексей.');
 });
 
-// Start the server
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
